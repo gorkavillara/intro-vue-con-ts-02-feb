7fd69599480725ebb1ce36ead45605f8534e0f95
gorkavillar <gorka@gorkavillar.com>
2023-02-02 12:23:00 +0100

    ⚠️ Tema 02 - Errores dinámicos y validación de inputs

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="main">
-  <!-- <h1>Mi contador</h1>
+    <!-- <h1>Mi contador</h1>
   <p>{{ contador }}</p>
   <button v-on:click="decrementa">-</button>
   <button @click="incrementa">+</button>
@@ -9,49 +9,79 @@
   {{ valorInput }}
   <br/> -->
     <input type="text" v-model="valorModel" />
-    {{ valorModel }}
+    <p :class="`failure small ${trueErrorMsg === '' ? 'hidden' : ''}`">
+      {{ trueErrorMsg }}
+    </p>
+    <p :class="`failure small ${errorMsg === '' ? 'hidden' : ''}`">
+      {{ errorMsg }}
+    </p>
+    <p>Valor: {{ valorModel }}</p>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { ref, Ref } from "vue"
-  const contador: Ref<number> = ref(0)
-  const valorInput = ref("Hola Jorge")
+import { ref, Ref, computed } from "vue";
 
-  const valorModel = ref("Hello Vue")
+const baseError = "ERROR: El texto debe superar los 10 caracteres";
 
-  const handleChange = (event: InputEvent) => {
-    console.log(event)
-    //@ts-ignore
-    valorInput.value = event.target?.value
-  }
+const contador: Ref<number> = ref(0);
+const valorInput = ref("Hola Jorge");
 
-  const incrementa = (e: PointerEvent) => {
-    console.log(e)
-    contador.value++
-    console.log("Click en incrementa", contador.value)
-  }
-  const decrementa = () => {
-    contador.value--
-    console.log("Click en decrementa", contador.value)
-  }
-  const log = (text: string) => {
-    console.log(text)
-  }
+const valorModel = ref("Hello Vue");
+const errorMsg = ref("");
+const trueErrorMsg = computed(() =>
+  valorModel.value.length < 10 ? baseError : ""
+);
 
-  const logEvent = (event: any) => console.log(event)
+const checkValidation = (event: InputEvent) => {
+  // Chequear si el texto es mayor a 10 caracteres
+  //@ts-ignore
+  const text = event.target.value as string;
+  if (text.length < 10) {
+    // Si no lo es, populamos el error
+    return (errorMsg.value = "ERROR: El texto debe superar los 10 caracteres");
+  }
+  return (errorMsg.value = "");
+};
+
+const handleChange = (event: InputEvent) => {
+  console.log(event);
+  //@ts-ignore
+  valorInput.value = event.target?.value;
+};
+
+const incrementa = (e: PointerEvent) => {
+  console.log(e);
+  contador.value++;
+  console.log("Click en incrementa", contador.value);
+};
+const decrementa = () => {
+  contador.value--;
+  console.log("Click en decrementa", contador.value);
+};
+const log = (text: string) => {
+  console.log(text);
+};
+
+const logEvent = (event: any) => console.log(event);
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .success {
-    color: green;
-  }
-  .failure {
-    color: red;
-  }
-  .main {
-    display: flex;
-    flex-direction: column;
-  }
+.success {
+  color: green;
+}
+.failure {
+  color: red;
+}
+.small {
+  font-size: 0.75rem;
+}
+.hidden {
+  display: none;
+}
+.main {
+  display: flex;
+  flex-direction: column;
+}
 </style>
