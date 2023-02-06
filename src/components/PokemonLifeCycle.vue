cebb9050a47917c8dce0d7a22c81cf958f34e2ff
gorkavillar <gorka@gorkavillar.com>
2023-02-06 10:20:08 +0100

    🐕 Tema 06 - Listado de pokemon con foto

--- src/components/PokemonLifeCycle.vue
+++ src/components/PokemonLifeCycle.vue
@@ -4,10 +4,16 @@
     <p>Cargando...</p>
   </div>
   <ul v-if="!loading && pokeList && pokeList.length > 0">
-    <li class="pokeCard" v-for="(pokemon, index) in pokeList" :key="index">
+    <li
+      class="pokeCard"
+      v-for="(pokemon, index) in pokeList"
+      :key="index"
+      @click="fetchSprite(pokemon)"
+    >
       <span class="id">{{ pokemon.id }}</span>
       <span class="name">{{ pokemon.name }}</span>
-      <span class="image">{{ pokemon.sprite }}</span>
+      <!-- <span class="image">{{ pokemon.sprite }}</span> -->
+      <img :src="pokemon.sprite" alt="" />
     </li>
   </ul>
 </template>
@@ -15,11 +21,35 @@
 <script lang="ts" setup>
 import { ref, Ref, onMounted } from "vue";
 import axios, { AxiosResponse } from "axios";
-import { Pokemon, PokeResponse } from "../models"
+import { Pokemon, PokeResponse } from "../models";
 import Loader from "./Loader.vue";
 
 const pokeList: Ref<Pokemon[]> = ref([]);
 const loading: Ref<boolean> = ref(false);
+
+const fetchSprite = (pokemon: Pokemon) => {
+  // 1 - Hacer una búsqueda del sprite del pokemon
+  axios
+    .get(pokemon.url)
+    .then(
+      (res: AxiosResponse<{ sprites: { front_default: string } }, any>) => {
+        pokeList.value = pokeList.value.map((storedPokemon: Pokemon) => {
+            if (storedPokemon.id === pokemon.id) {
+                // Pokemon al que le tenemos que poner el sprite
+                return { ...storedPokemon, sprite: res.data.sprites.front_default }
+            }
+            return storedPokemon
+            // return storedPokemon.id === pokemon.id ? {
+            //     ...storedPokemon,
+            //     sprite: res.data.sprites.front_default
+            // } : storedPokemon
+        })
+        console.log(pokeList.value)
+      }
+    );
+  // 2 - Introducir esta nueva información en la propiedad sprite
+  // 3 - Lo mostraremos en el DOM
+};
 
 onMounted(() => {
   console.log("Acabo de ser montado");
@@ -34,8 +64,9 @@ onMounted(() => {
       const lista: Pokemon[] = r.data.results.map((result, index) => ({
         id: index + 1,
         name: result.name,
+        url: result.url,
       }));
-      pokeList.value = lista
+      pokeList.value = lista;
     })
     .finally(() => {
       loading.value = false;
@@ -62,8 +93,17 @@ li.pokeCard {
   width: 100%;
   justify-content: space-between;
   align-items: center;
+  overflow: hidden;
 }
-li.image {
+span.id {
   width: 50px;
+  height: 100%;
+  display: grid;
+  place-content: center;
+  background-color: rgb(255, 247, 136);
+}
+img.image {
+  max-width: 50px;
+  overflow: hidden;
 }
 </style>
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,7 @@
 export interface Pokemon {
   id: number;
   name: string;
+  url: string;
   sprite?: string;
 }
 
