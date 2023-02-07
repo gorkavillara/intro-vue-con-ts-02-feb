3677452b5bc7de442a105013bd0fcf7c04b81dac
gorkavillar <gorka@gorkavillar.com>
2023-02-07 10:16:25 +0100

    🗒️ Tema 10 - Composable tareas

--- src/components/ListaTareas.vue
+++ src/components/ListaTareas.vue
@@ -1,18 +1,29 @@
 <template>
   <ul>
     <li v-for="(tarea, index) in tareas" :key="index">
-        {{ tarea.texto }}
+      <span
+        :style="`text-decoration: ${tarea.completado ? 'line-through' : ''}`"
+        >{{ tarea.texto }}</span
+      >
+      <button @click="() => completaTarea(index)">Completar</button>
+      <button @click="() => eliminaTarea(tarea.texto)">Eliminar</button>
     </li>
   </ul>
-  <input type="text" v-model="textoNuevaTarea" />
+  <input type="text" v-model="textoNuevaTarea" placeholder="Nueva tarea" />
+  <button @click="addNuevaTarea">Añadir tarea</button>
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue"
+import { ref } from "vue";
 import { useTareas } from "@/utils/tareasComposable";
 
-const { tareas, nuevaTarea } = useTareas();
+const { tareas, nuevaTarea, completaTarea, eliminaTarea } = useTareas();
 
-const textoNuevaTarea = ref("")
-console.log(tareas);
+const addNuevaTarea = () => {
+  if (textoNuevaTarea.value === "") return;
+  nuevaTarea(textoNuevaTarea.value, "Pablo");
+  textoNuevaTarea.value = "";
+};
+
+const textoNuevaTarea = ref("");
 </script>
